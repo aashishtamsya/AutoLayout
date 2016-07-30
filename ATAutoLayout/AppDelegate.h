e3f9a427703b46b6e7e93422d99d7501323b5a87
Aashish Tamsya <aashish.tamsya@gmail.com>
2016-07-30 12:35:58 +0530

    Initial commit

---
+++ ATAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATAutoLayout
+//
+//  Created by Aashish Tamsya on 30/07/16.
+//  Copyright © 2016 Aashish Tamsya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATAutoLayout
+//
+//  Created by Aashish Tamsya on 30/07/16.
+//  Copyright © 2016 Aashish Tamsya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
